9f33569c703b0c478febd0bc4b69fd02325e74dd
Duc <luuduc34@hotmail.com>
2023-06-30 11:09:44 +0200

    EVios1 / configure texfield with keyboard and switch

--- EVios1/EVios1/ViewController.swift
+++ EVios1/EVios1/ViewController.swift
@@ -10,14 +10,34 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var avatarContainer: UIView!
+    @IBOutlet var hidePass: UIView!
+    @IBOutlet var loginTextField: UITextField!
+    @IBOutlet var passTextField: UITextField!
+    @IBOutlet var loginBtn: UIButton!
+    @IBOutlet var switchBtn: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        // Frame avatar arrondi
         avatarContainer.layer.cornerRadius = avatarContainer.frame.height / 2
+        // Masquer le clavier sur un tap
+        let tapOnView = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tapOnView)
+        
+        hidePass.isUserInteractionEnabled = true
+        let clickPass = UITapGestureRecognizer(target: self, action: #selector(passButton))
+        hidePass.addGestureRecognizer(clickPass)
+        
+        loginTextField.keyboardType = .emailAddress
+        passTextField.keyboardType = .default
+        passTextField.isSecureTextEntry = true
     }
-
-
+    @objc func passButton(){
+        passTextField.isSecureTextEntry = false
+    }
+    @IBAction func login(){
+        
+    }
 }
 
